1ccb329d3bbfd6c8c63f4c3737322363e56edd4a
MersedSubasic <subasicmersed@gmail.com>
2023-02-17 13:31:52 -0500

    login page

---
+++ tetris-client/firebase/login.js
@@ -0,0 +1,82 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.5.0/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/9.5.0/firebase-auth.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDvPpc-1bT0OqmfkRH4nowl-7ffP140yww",
+    authDomain: "finaltetrisgamelogin.firebaseapp.com",
+    projectId: "finaltetrisgamelogin",
+    storageBucket: "finaltetrisgamelogin.appspot.com",
+    messagingSenderId: "323557137776",
+    appId: "1:323557137776:web:31381bfeac03ef3f5083b5"
+  };
+
+ const app = initializeApp(firebaseConfig);
+ const auth = getAuth(app);
+
+
+  document.getElementById("regButton").addEventListener('click', function(){
+   document.getElementById("signupdiv").style.display="inline";
+   document.getElementById("logindiv").style.display="none";
+});
+
+document.getElementById("signbtn").addEventListener('click', function(){
+ document.getElementById("signupdiv").style.display="none";
+ document.getElementById("logindiv").style.display="inline";
+
+});
+
+  document.getElementById("loginButton").addEventListener('click', function(){
+   const loginEmail= document.getElementById("loginEmail").value;
+   const loginPassword =document.getElementById("loginPassword").value;
+   const username =document.getElementById("name").value;
+
+   signInWithEmailAndPassword(auth, loginEmail, loginPassword)
+  .then((userCredential) => {
+    const user = userCredential.user;
+    document.getElementById("result-box").style.display="inline";
+     document.getElementById("logindiv").style.display="none";
+     window.location.replace("../public/index.html");
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    document.getElementById("result-box").style.display="inline";
+     document.getElementById("logindiv").style.display="none";
+     document.getElementById("result").innerHTML="Sorry ! <br>"+errorMessage;
+
+  });
+});
+
+
+  document.getElementById("signupButton").addEventListener('click', function(){
+
+   const signupEmail= document.getElementById("signupEmail").value;
+   const signupPassword =document.getElementById("signupPassword").value;
+   const username =document.getElementById("name").value;
+
+   createUserWithEmailAndPassword(auth, signupEmail, signupPassword)
+  .then((userCredential) => {
+    const user = userCredential.user;
+    document.getElementById("result-box").style.display="inline";
+     document.getElementById("signupdiv").style.display="none";
+     window.location.replace("../public/index.html");
+  }).catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    document.getElementById("result-box").style.display="inline";
+     document.getElementById("signupdiv").style.display="none";
+     document.getElementById("result").innerHTML="Sorry ! <br>"+errorMessage;
+
+  });
+});
+
+
+document.getElementById("logoutButton").addEventListener('click', function(){
+  signOut(auth).then(() => {
+     document.getElementById("result-box").style.display="none";
+       document.getElementById("logindiv").style.display="inline";
+  }).catch((error) => {
+     document.getElementById("result").innerHTML="Sorry ! <br>"+errorMessage;
+  });
+
+});

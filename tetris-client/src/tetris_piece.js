7f784bed20f0964628aca12b9ef0cb61b97d5018
PSTBOT <nk3286kp@go.minnstate.edu>
2023-02-17 22:02:12 -0600

    Show Random piece at start

--- tetris-client/src/tetris_piece.js
+++ tetris-client/src/tetris_piece.js
@@ -1,14 +1,29 @@
 class Tetris_piece {  
+  x;                                                                   // piece x position
+  y;                                                                   // piece y position
+  color;                                                               // color of the piece
+  shape;                                                               // shape of the piece
+  ctx;                                                                 // context for drawing
+  typeId;                                                              // type of the piece
+
+
   constructor(ctx) {
     this.ctx = ctx;
-    this.color = 'blue';
-    this.shape = [
-      [2, 0, 0], 
-      [2, 2, 2], 
-      [0, 0, 0]
-    ];
-    
-    // Starting position.
+
+
+    // This is HARD CODED for demo purposes
+
+    this.typeId = this.randomizeTetrominoType(COLORS.length - 1);      // random piece #
+    this.color = COLORS[this.typeId];                                  // color for selected piece
+    this.shape = SHAPES[this.typeId];                                  // shape for selected piece
+
+    // this.color = 'blue';
+    // this.shape = [
+    //   [2, 0, 0], 
+    //   [2, 2, 2], 
+    //   [0, 0, 0]
+    // ];
+                                                                       // Starting position.
     this.x = 4;
     this.y = 0;
   }
@@ -28,6 +43,18 @@ class Tetris_piece {
     });
   }
 
+  setStartingPosition() {                                              // Start based on block size
+    this.x = this.typeId === 4 ? 4 : 3;
+  }
 
+  randomizeTetrominoType(noOfTypes) {                                  // Return random block type
+    return Math.floor(Math.random() * noOfTypes + 1);
+  }
+
+  move(p) {                                                            // Move Piece to new position
+    this.x = p.x;
+    this.y = p.y;
+    this.shape = p.shape;
+  }
 
 }
